6b446d927e64fafc1e5856742b69f03d0a4c079c
Alexbybye <244417287@qq.com>
2026-01-20 08:17:05 +0800

    last commit for 1.0 shall it be

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,86 +1,68 @@
-import { createRouter, createWebHistory } from 'vue-router'
-// 注意：Vite 环境下导入 Vue 组件必须写 .vue 后缀
-import Animation1 from '../component/welcome/Animation1.vue'
-import Animation2 from '../component/welcome/Animation2.vue'
-import Animation3 from '../component/Main/Animation3.vue'
-import About from '../component/Pages/About.vue'
-import Article from '../component/Pages/Article.vue'
-import Music from '../component/Pages/Music.vue'
-import Album from '../component/Pages/Album.vue'
-import MusicPlaylist from '../component/Pages/MusicPlaylist.vue'
-import FriendLinks from '../component/Pages/Friendlinks.vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+// 导入组件
+import Animation3 from '@/component/Main/Animation3.vue'
+import About from '@/component/Pages/About.vue'
+import Album from '@/component/Pages/Album.vue'
+import AlbumDetail from '@/component/Pages/AlbumDetail.vue'
+import Article from '@/component/Pages/Article.vue'
+import Music from '@/component/Pages/Music.vue'
+import MusicPlaylist from '@/component/Pages/MusicPlaylist.vue'
+import TagCloud from '@/component/Pages/TagCloud.vue'
+import FriendLinks from '@/component/Pages/Friendlinks.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Animation3
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  },
+  {
+    path: '/album',
+    name: 'Album',
+    component: Album
+  },
+  {
+    path: '/album/:id',
+    name: 'AlbumDetail',
+    component: AlbumDetail,
+    props: true
+  },
+  {
+    path: '/article',
+    name: 'Article',
+    component: Article
+  },
+  {
+    path: '/music',
+    name: 'Music',
+    component: Music
+  },
+  {
+    path: '/musiclist',
+    name: 'MusicPlaylist',
+    component: MusicPlaylist
+  },
+  {
+    path: '/tagcloud',
+    name: 'TagCloud',
+    component: TagCloud
+  },
+  {
+    path: '/friends',
+    name: 'FriendLinks',
+    component: FriendLinks
+  }
+]
 
 const router = createRouter({
-    // 如果你部署在 AlexBybye.github.io 的根目录，传空字符串或 '/' 即可
-    history: createWebHistory(),
-    routes: [
-        {
-            path: '/',
-            name: 'animation1',
-            component: Animation1
-        },
-        {
-            path: '/animation2',
-            name: 'animation2',
-            component: Animation2
-        },
-        {
-            path: '/animation3',
-            name: 'animation3',
-            component: Animation3,
-            children: [
-                {
-                    path: 'about',
-                    name: 'about',
-                    component: About
-                },
-                {
-                    path: 'article',
-                    name: 'article',
-                    component: Article
-                },
-                {
-                    path: 'article/detail/:id',
-                    name: 'articleDetail',
-                    component: Article,
-                    props: true
-                },
-                {
-                    path: 'article/tag/:tag',
-                    name: 'articleByTag',
-                    component: Article,
-                    props: true
-                },
-                {
-                    path: 'music',
-                    name: 'music',
-                    component: Music
-                },
-                {
-                    path: 'music/playlist',
-                    name: 'musicPlaylist',
-                    component: MusicPlaylist
-                },
-                {
-                    path: 'album',
-                    name: 'album',
-                    component: Album
-                },
-                {
-                    path: 'album/detail/:id', 
-                    name: 'AlbumDetail',
-                    component: () => import('../component/Pages/AlbumDetail.vue'), // 所有相册共用这一个组件
-                    props: true
-                },
-                {
-                    path: 'friends',
-                    name: 'friendLinks',
-                    component: FriendLinks
-                }
-            ]
-        },
-
-    ],
+  history: createWebHashHistory('/PersonalWeb/'), // 使用hash模式
+  routes
 })
 
 export default router
