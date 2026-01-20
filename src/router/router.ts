fc4f25d9f39c31fb9853ef3b78d78fd4def2431b
Alexbybye <244417287@qq.com>
2026-01-20 08:43:56 +0800

    1

--- src/router/router.ts
+++ src/router/router.ts
@@ -61,7 +61,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory('/PersonalWeb/'), // 使用hash模式
+  history: createWebHashHistory(import.meta.env.BASE_URL), 
   routes
 })
 
